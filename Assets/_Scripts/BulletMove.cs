49326a42a31c95c23a55bbc14bd46bbb80b6a228
mrplcardoso <mrplcardoso@gmail.com>
2015-01-24 01:36:43 -0200

    Cena teste - criado bullet e camera

---
+++ Assets/_Scripts/BulletMove.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletMove : MonoBehaviour 
+{
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Translate(Vector3.forward * 5f * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		Debug.Log("Colidiu");
+	}
+}
---
+++ Assets/_Scripts/Shoot.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour
+{
+	public GameObject m_bullet;
+	public float shootTime = 5;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update() 
+	{
+		if(shootTime <= 0)
+		{
+			if(Input.GetKey(KeyCode.Space))
+			{
+				Instantiate(m_bullet, this.transform.position, 
+											GetComponentInParent<Transform>().transform.rotation);
+				shootTime = 10;
+			}
+		}
+		else
+		{
+			shootTime -= (Time.deltaTime * 10f);
+		}
+	}
+}
