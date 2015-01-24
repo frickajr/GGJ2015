cc3a6b9e0af8e706c581181de48d3c005806d7c4
Luiz Carlos Pinheiro Junior <frickajr@yahoo.com.br>
2015-01-24 04:09:47 -0200

    Acrescentado cogumelo.fbx e gerado cena teste

---
+++ Assets/_Scripts/loopTexture.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class loopTexture : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    public float scrollSpeed = 0.5F;
+    void Update()
+    {
+        float offset = Time.time * scrollSpeed;
+        renderer.material.SetTextureOffset("_MainTex", new Vector2(0, -offset));
+        renderer.material.SetTextureOffset("_BumpMap", new Vector2(0, -offset));
+
+    }
+}
---
+++ Assets/_Scripts/rotateDolphin.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotateDolphin : MonoBehaviour {
+
+    public float speed;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x,
+                                            transform.eulerAngles.y,
+                                            transform.eulerAngles.z + Time.time * speed);
+	}
+}
