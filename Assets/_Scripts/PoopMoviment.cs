82175490c8b0f02e163cb44c4519d98dde38111a
mrplcardoso <mrplcardoso@gmail.com>
2015-01-25 13:45:53 -0200

    scripts vassoura

---
+++ Assets/_Scripts/PoopMoviment.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PoopMoviment : MonoBehaviour
+{
+	public Vector3 rotPoop;
+	public float speed = 10;
+
+	// Use this for initialization
+	void Start()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (transform.position.y <= -10)
+			DestroyObject(gameObject);
+		transform.position += rotPoop * speed;
+	}
+}
---
+++ Assets/_Scripts/ShootVassoura.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootVassoura : MonoBehaviour
+{
+	public Vector3 targetPos;
+	public GameObject m_poop;
+	public float poopTime = 10;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if(poopTime <= 0)
+		{
+			Instantiate(m_poop, transform.position, Quaternion.identity);
+
+		}
+	}
+}
